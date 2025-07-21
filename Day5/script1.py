060410425c1f90b3c7b5e895caef48432aac8711
gauravms07 <gmshastry7@gmail.com>
2025-07-21 09:56:00 +0530

    updated day5

---
+++ Day5/script1.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder.appName("DataFrame Lab").master("local[*]").getOrCreate()
+
+print("Spark Session Created.")
+
+df = spark.range(1, 1000000).withColumn("squared", col("id") * col("id"))
+
+df.groupBy((col("id") % 10).alias("group")).count().show()
